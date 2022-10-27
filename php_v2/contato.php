e60bb199e643bbd8a46188b9488bfa0338bce306
Natanaele Machado <91093772+NatanaeleMachado@users.noreply.github.com>
2022-10-27 20:27:08 -0300

    Create contato.php

---
+++ php_v2/contato.php
@@ -0,0 +1,46 @@
+<?php 
+    include ("conectar.php");
+?>
+
+<!--INICIO CABEÇALHO-->
+<?php include('cabecalho.php'); ?>
+<!--FIM CABEÇALHO-->
+
+
+<!-- INICIO CONTATO -->
+
+<div class="container_contato">
+
+  <h3>Formulário de contato - AMIGOPET</a></h3><br><br>
+
+  Entre em contato através deste e-mail: <a href="amigopet@gmail.com">Enviar e-mail</a>
+
+  Caso houver algum problema ou sugestões, basta enviar o e-mail que iremos lhe responder
+</div>
+
+<!-- FIM CONTATO -->
+
+<!--INICIO RODAPE-->
+<footer>
+  <footer id="rodape">
+    <img class="logo2" alt="AMIGO PET" width="150x" src="img/logo.png" />
+
+    <div class="sitemap">
+      <li><a href="home.html">Home</a></li>
+      <li><a href="contato.html">Contato</a></li>
+      <li><a href="sobre.html">Sobre</a></li>
+      <li><a href="parceiro.html">Parceiro</a></li>
+      <li><a href="login.html">Cadastrar | Login</a></li>
+    </div>
+
+    <div class="direitos_autorais">
+      Instituto Amigo Pet © 2022 Todos os direitos reservados.
+      Consulte nossa Política de Privacidade.
+    </div>
+  </footer>
+  <!--FIM RODAPE-->
+
+
+  </body>
+
+  </html>
