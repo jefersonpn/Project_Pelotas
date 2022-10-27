ab784ee7c33370126cf71c716c048e82dfa70303
Natanaele Machado <91093772+NatanaeleMachado@users.noreply.github.com>
2022-10-27 20:49:13 -0300

    Create tipo_usuario.php

---
+++ php_v2/CADASTRO/tipo_usuario.php
@@ -0,0 +1,23 @@
+<?php 
+    include ("conectar.php");
+?>
+
+<!--INICIO CABEÇALHO-->
+<?php include('cabecalho.php'); ?>
+<!--FIM CABEÇALHO-->
+
+    <!-- INICIO LOGIN -->
+
+    <div class="container_login">
+
+        <a href="cadastro/form_cad_protetor.php"><button id="button">Sou Protetor</button></a><br><br>
+        <a href="cadastro/form_cad_doador.php"><button id="button">Sou Doador</button></a><br><br>
+        <a href="cadastro/form_cad_parceiro.php"><button id="button">Sou Parceiro</button></a>
+        
+    </div>
+
+    <!-- FIM LOGIN -->
+
+<!--INICIO RODAPE -->
+        <?php include('rodape.php'); ?>
+<!--FIM RODAPE -->
