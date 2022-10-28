79a3c2f3f987a83c9269ba95e3622c0295ccf010
Natanaele Machado <91093772+NatanaeleMachado@users.noreply.github.com>
2022-10-27 21:29:50 -0300

    Update form_cad_protetor.php

--- php_v2/CADASTRO/form_cad_protetor.php
+++ php_v2/CADASTRO/form_cad_protetor.php
@@ -1,9 +1,9 @@
 <?php 
-    include ("conectar.php");
+    include("../conectar.php");
 ?>
 
 <!--INICIO CABEÇALHO-->
-<?php include('cabecalho.php'); ?>
+<?php include('../cabecalho.php'); ?>
 <!--FIM CABEÇALHO-->
 
 
@@ -12,67 +12,55 @@
  <div class="container_cadastro">
 
         <h3>Realize seu Cadastro Protetor ;)</h3>
-	 
-	 	<form name="foto_usuario" type="text" method="post" enctype="multipart/form-data" action="upload.php">
-                    <label>
-                        <p>Foto</p> <br><input type="file" name="image" />
-                        <!-- <input type="submit" id="button_upload" value="Enviar"
-                                name="envia" /> -->
-                </form><br><br><br>
 
-        <form method="post" action="form_cad_protetor.php">
+        <form method="post" action=""><br><br>
 
-            <label>Nome
-            <input type="text" name="nome" placeholder="Nome Completo" required><br><br>
-            </label>
+        <!-- protetor -->
+        <div>
+            <label>Nome</label>
+            <input type="text" name="nome" placeholder="Nome Completo"><br><br>
 
-            <label>E-mail
-            <input type="text" name="email" placeholder="Digite seu email" required><br><br>
-            </label>
+            <label>E-mail</label>
+            <input type="text" name="email" placeholder="Digite seu email"><br><br>
 
-            <label>Telefone
-            <input type="text" name="telefone" placeholder="Digite seu telefone" required><br><br>
-            </label>
+            <label>Telefone</label>
+            <input type="text" name="telefone" placeholder="Digite seu telefone"><br><br>
 
-            <label>Cidade
-            <input type="text" name="cidade" placeholder="Digite a sua cidade" required><br><br>
-            </label>
+            <label>Cidade</label>
+            <input type="text" name="cidade" placeholder="Digite a sua cidade"><br><br>
 
-            <label>Login
-            <input type="text" name="login" placeholder="Digite seu login" required><br><br>
-            </label>
+            <label>Login</label>
+            <input type="text" name="login" placeholder="Digite seu login"><br><br>
 
-            <label>Senha
-            <input type="text" name="senha" placeholder="Digite a senha" required><br><br>
-            </label>
+            <label>Senha</label>
+            <input type="text" name="senha" placeholder="Digite a senha"><br><br>
+        </div>
 
-            <br><button type="submit" name="enviar" value="1" id="form_botao_cadastrar">Cadastrar Protetor</button>
-			&nbsp&nbsp&nbsp<a href="index.html"><input type="button" value="Voltar"></a>
+        <br><button type="submit" name="enviar" id="button">Cadastrar Protetor</button>
 
         </form> 
 
     <!--INICIO RODAPE -->
 
-        <?php include('rodape.php'); ?>
+        <?php include('../rodape.php'); ?>
 
     <!--FIM RODAPE -->
-     
-     
-     <!-- Conexão php -->
+
+    <!-- conexão PHP -->
 <?php
     include_once("conectar.php");
     if(isset($_POST['Post'])){
         $nome=$_POST['nome'];
         $email=$_POST['email'];
-        $telefone=$POST['telefone'];
+        $telefone=$_POST['telefone'];
         $cidade=$_POST['cidade'];
         $login=$_POST['login'];
         $senha=$_POST['senha'];
+        //$imagem=$_POST['imagem'];
 
-        $sql = "INSERT INTO protetor (nome, email, telefone, cidade, login_protetor, senha_protetor) VALUES ('$nome','$email','$telefone','$cidade', '$login', '$senha', NOW())";
-
+    $sql = "INSERT INTO protetor (nome, email, telefone, cidade, login_protetor, senha_protetor) VALUES ('$nome','$email','$telefone','$cidade', '$login', '$senha', NOW())";
         mysqli_query($conexao,$sql);
         mysqli_close($conexao);
-        header('Location: lista_protetor.php');
+        header('Location: dashboard/painel_protetor.php');
     }
 ?>
