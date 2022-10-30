3c3c7c0fe82cde4d33681caa3ccb2790ae0b5421
jefersonpn <jefersonpn@gmail.com>
2022-10-30 08:26:50 +0100

    Alterando cabeçalho source

--- php_v2/CADASTRO/form_cad_doador.php
+++ php_v2/CADASTRO/form_cad_doador.php
@@ -3,64 +3,64 @@
 ?>
 
 <!--INICIO CABEÇALHO-->
-<?php include('../cabecalho.php'); ?>
+<?php include('../cabecalho_outros.php'); ?>
 <!--FIM CABEÇALHO-->
 
-    <!--FORMULÁRIO DE CADASTRO-->
+<!--FORMULÁRIO DE CADASTRO-->
 
- <div class="container_cadastro">
+<div class="container_cadastro">
 
-        <h3>Realize seu Cadastro Doador ;)</h3>
+  <h3>Realize seu Cadastro Doador ;)</h3>
 
-        <form method="post" action=""><br><br>
-		
+  <form method="post" action=""><br><br>
+
     <!-- doador -->
-        <form method="post" action="form_cad_doador.php">
+    <form method="post" action="form_cad_doador.php">
 
-            <label>Nome
-            <input type="text" name="nome" placeholder="Nome Completo" required><br><br>
-            </label>
+      <label>Nome
+        <input type="text" name="nome" placeholder="Nome Completo" required><br><br>
+      </label>
 
-            <label>E-mail
-            <input type="text" name="email" placeholder="Digite seu email" required><br><br>
-            </label>
+      <label>E-mail
+        <input type="text" name="email" placeholder="Digite seu email" required><br><br>
+      </label>
 
-            <label>Telefone
-            <input type="text" name="telefone" placeholder="Digite seu telefone" required><br><br>
-            </label>
+      <label>Telefone
+        <input type="text" name="telefone" placeholder="Digite seu telefone" required><br><br>
+      </label>
 
-            <label>Cidade
-            <input type="text" name="cidade" placeholder="Digite a sua cidade" required><br><br>
-            </label>
+      <label>Cidade
+        <input type="text" name="cidade" placeholder="Digite a sua cidade" required><br><br>
+      </label>
 
-            <label>Login
-            <input type="text" name="login" placeholder="Digite seu login" required><br><br>
-            </label>
+      <label>Login
+        <input type="text" name="login" placeholder="Digite seu login" required><br><br>
+      </label>
 
-            <label>Senha
-            <input type="text" name="senha" placeholder="Digite a senha" required><br><br>
-            </label>
-		
-	    <form name="foto_usuario" type="text" method="post" enctype="multipart/form-data" action="upload.php">
-                    <label>
-                        <p>Foto</p> <br><input type="file" name="image" />
-                        <input type="submit" id="button_upload" value="Enviar" name="envia" /> 
-             </form><br><br><br>		
-		
-		
+      <label>Senha
+        <input type="text" name="senha" placeholder="Digite a senha" required><br><br>
+      </label>
 
-            <br><button type="submit" name="enviar" value="1" id="form_botao_cadastrar">Cadastrar Doador</button> 
+      <form name="foto_usuario" type="text" method="post" enctype="multipart/form-data" action="upload.php">
+        <label>
+          <p>Foto</p> <br><input type="file" name="image" />
+          <input type="submit" id="button_upload" value="Enviar" name="envia" />
+      </form><br><br><br>
 
-        </form> 
+
+
+      <br><button type="submit" name="enviar" value="1" id="form_botao_cadastrar">Cadastrar Doador</button>
+
+    </form>
 
     <!--INICIO RODAPE -->
 
-        <?php include('../rodape.php'); ?>
+    <?php include('../rodape.php'); ?>
 
     <!--FIM RODAPE -->
 
     <!-- conexão PHP -->
-<?php
+    <?php
     include_once("../conectar.php");
     if(isset($_POST['Post'])){
         $nome=$_POST['nome'];
