12ef144a43c95f12da65dabe57249d71b8489458
jefersonpn <jefersonpn@gmail.com>
2022-10-30 08:25:48 +0100

    Remocao de TAG a mais

--- php_v2/rodape.php
+++ php_v2/rodape.php
@@ -26,28 +26,27 @@
     ?>
 
 
-    <!--INICIO RODAPE-->
-    <footer>
+        <!--INICIO RODAPE-->
         <footer id="rodape">
-            <img class="logo2" alt="AMIGO PET" width="150x" src="img/logo.png" />
+          <img class="logo2" alt="AMIGO PET" width="150x" src="../img/logo.png" />
 
-            <div class="sitemap">
-              <ul class="menu">
-                <li><a href="index.php?title=home">Home</a></li>
-                <li><a href="contato.php?title=contato">Contato</a></li>
-                <li><a href="sobre.php?title=sobre">Sobre</a></li>
-                <li><a href="parceiro.php?title=parceiro">Parceiro</a></li>
-                <li><a href="login.php?title=login">Cadastrar | Login</a></li>
-             </ul>
-            </div>
+          <div class="sitemap">
+            <ul class="menu">
+              <li><a href="index.php?title=home">Home</a></li>
+              <li><a href="contato.php?title=contato">Contato</a></li>
+              <li><a href="sobre.php?title=sobre">Sobre</a></li>
+              <li><a href="parceiro.php?title=parceiro">Parceiro</a></li>
+              <li><a href="login.php?title=login">Cadastrar | Login</a></li>
+            </ul>
+          </div>
 
-            <div class="direitos_autorais">
-                Instituto Amigo Pet © 2022 Todos os direitos reservados.
-                Consulte nossa Política de Privacidade.
-            </div>
+          <div class="direitos_autorais">
+            Instituto Amigo Pet © 2022 Todos os direitos reservados.
+            Consulte nossa Política de Privacidade.
+          </div>
         </footer>
         <!--FIM RODAPE-->
 
-</body>
+        </body>
 
-</html>
+        </html>
