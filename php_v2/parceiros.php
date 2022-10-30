cdf5a5478470b9c3a7fc40869400e9afb7d6139d
jefersonpn <jefersonpn@gmail.com>
2022-10-30 10:11:32 +0100

    adicionando logout e parceiros

---
+++ php_v2/parceiros.php
@@ -0,0 +1,102 @@
+<?php 
+    include("conectar.php");
+?>
+
+<!--INICIO CABEÇALHO-->
+<?php include('cabecalho.php'); ?>
+<!--FIM CABEÇALHO-->
+
+<!-- INICIO PARCEIRO -->
+
+<img class="imagem_banner" src="img/parceiro/amigopet_parceiro03.png" width="900x" alt=""><br><br>
+
+<section class="posicao_padrao">
+
+  <div>
+    <h3>Seja uma empresa parceira e faça a diferença com a
+      gente!</h3><br>
+
+    <p class="paragrafo_especial">A parceria perfeita para o seu programa de
+      Responsabilidade Social! :)</p><br>
+  </div><br>
+
+</section>
+
+<h3 class="titulo_parceiro">PARCEIROS</h3><br>
+
+
+<section class="container_ParceiroHome">
+
+  <div class="item">
+    <img src="img/parceiro/amigopet_parceiro01.png" width="100x" alt="">
+  </div>
+
+  <div class="item">
+    <p>
+      Estamos a mais 6 anos atuando no Mercado Livre, mais de<br>
+      15.000 vendas concretizadas, agora com um novo site feito<br>
+      especial para nossos clientes, com o melhor atendimento<br>
+      e preços.
+    </p>
+  </div>
+
+  <div class="itemHome">
+    <a href="https://abre.ai/eWNl" class="button">Localização</a>
+  </div>
+
+</section>
+
+<section class="container_ParceiroHome">
+
+  <div class="item">
+    <img src="img/parceiro/amigopet_parceiro04.png" width="100x" alt=""><br><br>
+  </div>
+
+  <div class="item">
+    <p>Sallaberry está no mercado desde 1994, abastecendo a cidade<br>
+      de Pelotas e região com produtos agropecuários, petshop,<br>
+      ferragem e bazar.
+    </p>
+  </div>
+
+  <div class="itemHome">
+    <a href="https://abre.ai/eWNx" class="button">Localização</a>
+  </div>
+
+</section>
+
+<section class="container_ParceiroHome">
+
+  <div class="item">
+    <img src="img/parceiro/amigopet_parceiro02.png" width="100x" alt=""><br><br>
+  </div>
+
+  <div class="item">
+    <p>
+      Agropecuária e Pet shop com foco em ração extra limpa para<br>
+      pássaros, com produção própria, visando o bem estar dos animais.
+    </p>
+  </div>
+
+  <div class="itemHome">
+    <a href="https://abre.ai/eWNB" class="button">Localização</a>
+  </div>
+
+</section><br><br>
+
+<h3 class="titulo_parceiro">Vamos conversar?</h3>
+
+<p class="texto_parceiro">Escreva para <strong><span style="color: #FF6315;">
+      parcerias_amigopet@gmail.com</span></strong> ou whatsapp
+  <strong><span style="color: #FF6315;">(53) 98425-7240</span></strong>.<br>
+  A bicharada por aqui está animada, e nós também. =^.^=
+</p><br>
+
+</div>
+<!-- FIM PARCEIRO -->
+
+<!--INICIO RODAPE -->
+
+<?php include('rodape.php'); ?>
+
+<!--FIM RODAPE -->
