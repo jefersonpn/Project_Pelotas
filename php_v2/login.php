f620c5f60b7d87bc8c9f5364980a7fd5d2a4a336
Natanaele Machado <91093772+NatanaeleMachado@users.noreply.github.com>
2022-11-06 20:08:19 -0300

    Update login.php

--- php_v2/login.php
+++ php_v2/login.php
@@ -1,5 +1,5 @@
 <?php
-// colocar no git
+
 include('conectar.php');
 
 if(isset($_POST['email']) || isset($_POST['senha'])) {
@@ -51,29 +51,28 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
     <form method="post" action="fazer_login.php?login=1">
 
       <h3>AMIGOPET</h3>
-      <h4>PROTETOR</h4>
+      <h4>PROTETOR</h4><br>
 
-      <p class="link">Faça o seu login<br><br><br>
-
-        Ainda não tem conta?
+       Ainda não tem conta?
         <a href="form_cad_protetor.php">Cadastre-se</a>
-      </p><br><br>
-
-      <p>
-        <label for="email_login">E-mail&nbsp;</label>
+      </p><br>
+      <br><br>
+        
+      <div style="margin-top:-80px;">
+        <label for="email_login">E-mail&nbsp;</label><br>
         <input id="email_login" name="email_login" required="required" type="text"
           placeholder="contato@amigopet.com" /><br><br>
-      </p>
 
-      <p>
-        <label for="senha_login">Senha&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        <input id="senha_login" name="senha_login" required="required" type="password" placeholder="senha" />
-      </p><br><br>
 
-      <p>
-        <input class="button" type="submit" value="Logar" />
-      </p><br>
 
+        <label for="senha_login">Senha&nbsp;&nbsp;&nbsp;&nbsp;</label><br>
+        <input id="senha_login" name="senha_login" required="required" type="password" placeholder="senha" /><br>
+
+
+        <p><br>
+          <input class="btn btn-success" type="submit" value="Logar" />
+        </p><br>
+      </div>
     </form>
   </div>
 
@@ -82,28 +81,23 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
 
       <h3>AMIGOPET</h3>
       <h4>DOADOR</h4>
-
-      <p class="link">Faça o seu login<br><br><br>
-
-        Ainda não tem conta?
+      <br>
+      Ainda não tem conta?
         <a href="form_cad_doador.php">Cadastre-se</a>
-      </p><br><br>
+      </p><br>
+      <br>
 
-      <p>
-        <label for="email_login">E-mail&nbsp;</label>
+      <div style="margin-top:-80px;"><br>
+        <label for="email_login">E-mail&nbsp;</label><br>
         <input id="email_login" name="email_login" required="required" type="text"
           placeholder="contato@amigopet.com" /><br><br>
-      </p>
 
-      <p>
-        <label for="senha_login">Senha&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        <input id="senha_login" name="senha_login" required="required" type="password" placeholder="senha" />
-      </p><br><br>
-
-      <p>
-        <input class="button" type="submit" value="Logar" />
-      </p><br>
-
+        <label for="senha_login">Senha&nbsp;&nbsp;&nbsp;&nbsp;</label><br>
+        <input id="senha_login" name="senha_login" required="required" type="password" placeholder="senha" /><br>
+        <p><br>
+          <input class="btn btn-success" type="submit" value="Logar" />
+        </p><br>
+      </div>
     </form>
   </div>
 
@@ -112,26 +106,24 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
 
       <h3>AMIGOPET</h3>
       <h4>PARCEIRO</h4>
-
-      <p class="link">Faça o seu login<br><br><br>
+      <br>
 
         Ainda não tem conta?
         <a href="form_cad_parceiro.php">Cadastre-se</a>
-      </p><br><br>
+      </p><br>
+      <br><br>
 
-      <p>
-        <label for="email_login">E-mail&nbsp;</label>
+      <div style="margin-top:-80px;">
+        <label for="email_login">E-mail&nbsp;</label><br>
         <input id="email_login" name="email_login" required="required" type="text"
           placeholder="contato@amigopet.com" /><br><br>
-      </p>
 
-      <p>
-        <label for="senha_login">Senha&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        <input id="senha_login" name="senha_login" required="required" type="password" placeholder="senha" />
-      </p><br><br>
+        <label for="senha_login">Senha&nbsp;&nbsp;&nbsp;&nbsp;</label><br>
+        <input id="senha_login" name="senha_login" required="required" type="password" placeholder="senha" /><br>
+      </div>
 
-      <p>
-        <input class="button" type="submit" value="Logar" />
+      <p><br>
+        <input class="btn btn-success " type="submit" value="Logar" />
       </p><br>
 
     </form>
