48aca86aecd3071be4a91aaf82a0554c28b46720
Natanaele Machado <91093772+NatanaeleMachado@users.noreply.github.com>
2022-11-06 22:02:54 -0300

    Update form_cad_parceiro.php

--- php_v2/form_cad_parceiro.php
+++ php_v2/form_cad_parceiro.php
@@ -7,50 +7,46 @@
 <!--FIM CABEÇALHO-->
 
 
-<!--FORMULÁRIO DE CADASTRO-->
+<!--FORMULÁRIO DE CADASTRO parceiro-->
 
 <div class="container_cadastro">
 
-  <h3>Realize seu Cadastro Parceiro ;)</h3><br>
+  <div class="form_cadastro">
+  <h3>Realize seu Cadastro Parceiro</h3><br>
 
   <form method="post" action="insert_parceiro.php" enctype="multipart/form-data">
 
-    <label>Nome Fantasia
-      <input type="text" name="nomefantasia" placeholder="Nome Fantasia" required><br><br>
-    </label>
+    <label>Nome Fantasia <input type="text" name="nomefantasia" placeholder="Nome Fantasia" required>
+    </label><br><br>
 
-    <label>E-mail
-      <input type="text" name="email" placeholder="Digite seu email" required><br><br>
-    </label>
+    <label>E-mail <input type="text" name="email" placeholder="Digite seu email" required>
+    </label><br><br>
 
-    <label>Telefone
-      <input type="text" name="telefone" placeholder="Digite seu telefone" required><br><br>
-    </label>
+    <label>Telefone <input type="text" name="telefone" placeholder="Digite seu telefone" required>
+    </label><br><br>
 
-    <label>Cidade
-      <input type="text" name="cidade" placeholder="Digite a sua cidade" required><br><br>
-    </label>
+    <label>Cidade <input type="text" name="cidade" placeholder="Digite a sua cidade" required>
+    </label><br><br>
 
-    <label>CNPJ
-      <input type="text" name="cnpj" placeholder="Digite seu cnpj" required><br><br>
-    </label>
+    <label>CNPJ <input type="text" name="cnpj" placeholder="Digite seu cnpj" required>
+    </label><br><br>
 
-    <label>Login
-      <input type="text" name="login" placeholder="Digite seu login" required><br><br>
-    </label>
+    <label>Login <input type="text" name="login" placeholder="Digite seu login" required>
+    </label><br><br>
 
-    <label>Senha
-      <input type="text" name="senha" placeholder="Digite a senha" required><br><br>
-    </label>
+    <label>Senha <input type="text" name="senha" placeholder="Digite a senha" required>
+    </label><br><br>
 
 
-    <label>
+    <label><!-- 
       <p>Sua Foto</p> <br><input type="file" name="image" />
-      </br>
+      </br>-->
 
-      <br><button type="submit" name="enviar" value="1" id="form_botao_cadastrar">Cadastrar Protetor</button>
-
+      <br><button type="submit" name="enviar" value="1" id="button">Cadastrar Protetor</button>
+      </div>
   </form>
+
+  <!-- botão: form_botao_cadastrar -->
 
   <!--INICIO RODAPE -->
   <?php include('rodape.php'); ?>
