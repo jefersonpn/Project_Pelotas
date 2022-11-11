ab2b21b93a4c1cf104f02ca4736c2c4f9c34b1e1
Natanaele Machado <91093772+NatanaeleMachado@users.noreply.github.com>
2022-11-10 22:01:56 -0300

    Update cadastro_doacao_solicitacao.php

--- php_v2/cadastro_doacao_solicitacao.php
+++ php_v2/cadastro_doacao_solicitacao.php
@@ -6,50 +6,91 @@ include_once("conectar.php");
 <?php include('cabecalho.php'); ?>
 <!--FIM CABEÇALHO-->
 
-      <head>
-        <title>AmigoPet - Solicitação</title>
-        <link rel="stylesheet" type="text/css" href="formulario.css" />
-      </head>
+<?php
+  session_start();
+  //print_r($_SESSION['usuario_logado']); 
+  
+?>
 
-      <body>
+<!--FORMULÁRIO DE CADASTRO-->
+<div class="container">
+  <div class=" text-center mt-5 ">
+    <h1>Cadastro de Solicitação - AMIGOPET</h1>
+  </div>
+  <form method="post" action="insert_doacao.php" enctype="multipart/form-data" onsubmit="validaForm(); return false;"
+    class="form">
+    <div class="row ">
+      <div class="col-lg-7 mx-auto">
+        <div class="card mt-2 mx-auto p-4 bg-light">
+          <div class="card-body bg-light">
 
-        <div class="cadastrar_doacoes_solicitacoes">
+            <div class="container">
+              <form id="contact-form" role="form">
 
-        <header>
-          <h3>Cadastrar Solicitação</h3>
-        </header><br>
 
-        <form method="post" action="cadastro_doacao_voluntaria.php">
 
-          <label>
-            <p>Nome</p> <input type="text" name="descricao" id="id_nome" placeholder="Nome do Objeto"
-              size="25">
-          </label><br><br>
+                <div class="controls">
 
-          <label>
-            <p>Descrição</p> 
-            <textarea name="motivo" rows="5" cols="52"></textarea>
-          </label><br>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <label for="descricao">Descrição *</label>
+                        <input id="descricao" type="text" name="descricao" class="form-control"
+                          placeholder="Descrição do item. *" required="required" data-error="Descrição é obrigatório.">
 
-          <label><br>
-            <p>Quantidade</p><input type="number" name="quantidade">
-          </label><br>
+                      </div>
+                    </div>
+                  </div>
 
-          <br><label>
-            <p>Parceiros</p>
-            <select name="categoria" required>
-            </select><br><br><br><br>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="quantidade">Quantidade *</label>
+                        <input id="quantidade" type="number" name="quantidade" class="form-control" required="required"
+                          data-error="Quantidade é  obrigatório.">
 
-            <button type="submit" name="Post" value="1">Solicitar</button>&nbsp&nbsp&nbsp&nbsp&nbsp<a
-              href="index.html"><input type="button" value="Voltar"></a>
+                      </div>
+                    </div>
+                    </div>
+                    </div>
+                  </div>
 
-        </form>
-        </div>
+                  </br>
+                  <div class=" row">
+                    <!-- UPLOAD Image -->
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <label for="imagem">Foto</label>
+                        <input id="imagem" type="file" name="imagem" class="form-control p-1">
+                      </div>
+                    </div>
+                  </div>
 
-    <!--INICIO RODAPE -->
-    <?php include('rodape.php'); ?>
-    <!--FIM RODAPE -->
+                  <div class="col-md-12">
+                    <input type="submit" class="btn btn-success btn-send  pt-2 btn-block
+                            " name="submit" value="Cadastrar">
+                  </div>
+                </div>
+            </div>
+  </form>
+</div>
+</div>
 
-    </body>
 
-    </html>
+</div>
+<!-- /.8 -->
+
+</div>
+<!-- /.row-->
+
+</div>
+</div>
+
+<!--INICIO RODAPE -->
+<?php include('rodape.php'); ?>
+<!--FIM RODAPE -->
+<div class="container_contato">
+
+  </body>
+
+  </html>
