121e7fb8b9d53457b2bc73f9062c84d29c370ecc
jefersonpn <jefersonpn@gmail.com>
2022-11-21 15:28:15 +0100

    Adicionando limite de coleta

--- php_v2/coletar_doacao.php
+++ php_v2/coletar_doacao.php
@@ -25,55 +25,85 @@ $sql_doacao= "Select * from `doacao_voluntaria` where `id_doacao`= $id_doacao" ;
                     $qtd_coleta= $row['qtd_coleta'];
                   }
 
+
+// Pega a data atual em segundos
+$hoje= time();
+// pega apenas o mês atual
+$mesAtual= (date("m",$hoje));
+// Pega a data atual em segundos
+    
+//
+$qtd_coletada= '';
+
+$sql= "SELECT fk_protetor_id AS protetor, id_solicitacao AS solicitacao, EXTRACT(MONTH FROM data_cadastro) AS month FROM doacao_solicitacao WHERE fk_protetor_id = $id_protetor" ;
+$result=mysqli_query($conn, $sql);
+
+while($row=mysqli_fetch_assoc($result)) 
+{
+  $id_solicitacao=$row['solicitacao']; 
+  $fk_protetor_id=$row['protetor']; 
+  $mes=$row['month'];
+  
+ // echo "Solicitacao => ".$id_solicitacao."<br>Protetor =>  ".$fk_protetor_id." <br> Mes=> ".$mes."<br><br>";
+
+  if($mes == $mesAtual){
+    //echo "Coleta do mês => ".$mesAtual."<br>";
+    $qtd_coletada++;
+  }
+
+  //echo "Solicitacao => ".$id_solicitacao."<br>Protetor =>  ".$fk_protetor_id." <br> Mes=> ".$mes."<br>Quantidade coletada atualmente: ".$qtd_coletada."<br>";
+}
+//echo "QTD => ".$qtd_coletada."<br>"; 
+
 //echo "id_doacao= $id_doacao, id_parceiro= $id_parceiro, id_protetor= $id_protetor, id_doador= $id_doador <br>";
 
-
-// Inserindo na  lista dos meus coletados.
-$sqlquery = "INSERT INTO doacao_solicitacao (fk_doacao_id, fk_parceiro_id, fk_protetor_id, fk_doador_id) VALUES
-('$id_doacao', '$id_parceiro', '$id_protetor', '$id_doador')";
-
-if ($conn->query($sqlquery) === TRUE) {
-
-} else {
-echo "Error: " . $sqlquery . "<br>" . $conn->error;
-}
-
-// Alterando o statos do item para reservado.
-$status = "5";
-
-$sqlquery = "UPDATE `doacao_voluntaria` SET `fk_status_id`= $status WHERE `id_doacao`= $id_doacao"; 
-
-if ($conn->query($sqlquery) === TRUE) 
+// Coletar item apenas se tiver 2 ou menos no mes ja coletado.
+if ($qtd_coletada < 2)
 {
-    // Verificando se o PROTETOR ja coletou mais de 2 no mes
-    // Tirando 1 da coluna qtd_coleta caso. protetor desista da doacao 
-    $sql_doacao= "Select * from `protetor` where `id_protetor`= $id_protetor" ;
-                  $result=mysqli_query($conn, $sql_doacao);
-                  while($row=mysqli_fetch_assoc($result)) 
-                  {
-                    $id_protetor=$row['id_protetor']; 
-                    $qtd_coleta= $row['qtd_coleta'];
-                  }
+    // Inserindo na  lista dos meus coletados.
+  $sqlquery = "INSERT INTO doacao_solicitacao (fk_doacao_id, fk_parceiro_id, fk_protetor_id, fk_doador_id) VALUES
+  ('$id_doacao', '$id_parceiro', '$id_protetor', '$id_doador')";
 
-    $qtd_coleta++;
+  if ($conn->query($sqlquery) === TRUE) {
 
-    $sqlquery = "UPDATE `protetor` SET `qtd_coleta`= $qtd_coleta WHERE `id_protetor`= $id_protetor"; 
-
-    if ($conn->query($sqlquery) === FALSE) 
-    {
-    echo "Error: " . $sqlquery . "<br>" . $conn->error;
-    }  
-  header("location: meus_coletados.php");
-
-}else {
+  } else {
   echo "Error: " . $sqlquery . "<br>" . $conn->error;
+  }
+
+  // Alterando o statos do item para reservado.
+  $status = "5";
+
+  $sqlquery = "UPDATE `doacao_voluntaria` SET `fk_status_id`= $status WHERE `id_doacao`= $id_doacao"; 
+
+  if ($conn->query($sqlquery) === TRUE) 
+  {
+      // Verificando se o PROTETOR ja coletou mais de 2 no mes
+      // Tirando 1 da coluna qtd_coleta caso. protetor desista da doacao 
+      $sql_doacao= "Select * from `protetor` where `id_protetor`= $id_protetor" ;
+                    $result=mysqli_query($conn, $sql_doacao);
+                    while($row=mysqli_fetch_assoc($result)) 
+                    {
+                      $id_protetor=$row['id_protetor']; 
+                      $qtd_coleta= $row['qtd_coleta'];
+                    }
+
+      $qtd_coleta++;
+
+      $sqlquery = "UPDATE `protetor` SET `qtd_coleta`= $qtd_coleta WHERE `id_protetor`= $id_protetor"; 
+
+      if ($conn->query($sqlquery) === FALSE) 
+      {
+      echo "Error: " . $sqlquery . "<br>" . $conn->error;
+      }  
+    header("location: meus_coletados.php");
+
+  }else {
+    echo "Error: " . $sqlquery . "<br>" . $conn->error;
+  }
+
+}else{
+    header("location: limite_de_coleta.php");
 }
-
-
-
-
-
-
 
 
 ?>
---
+++ php_v2/limite_de_coleta.php
@@ -0,0 +1,17 @@
+<?php 
+    include ("conectar.php");
+?>
+
+<!--INICIO CABEÇALHO-->
+<?php include('cabecalho.php'); ?>
+<!--FIM CABEÇALHO-->
+
+<div class="h3 p-5">Voce ja coletou 2 itens esse mes. <a href='<?php echo $anterior ?>'
+    class="btn btn-danger">Voltar</a>
+</div>
+
+<!--INICIO RODAPE -->
+
+<?php include('rodape.php'); ?>
+
+<!--FIM RODAPE -->
